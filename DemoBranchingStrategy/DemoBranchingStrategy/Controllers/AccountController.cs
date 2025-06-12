ebd4db324e75b0b2585a83fbb96afd2c1b4592d5
nitin <nitin@thinkaicorp.com>
2025-06-12 18:16:51 +0530

    commit implement get accounts

---
+++ DemoBranchingStrategy/DemoBranchingStrategy/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoBranchingStrategy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetAccounts")]
+        public IEnumerable<WeatherForecast> GetAccounts()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
--- DemoBranchingStrategy/DemoBranchingStrategy/obj/Debug/net8.0/DemoBranchingStrategy.AssemblyInfo.cs
+++ DemoBranchingStrategy/DemoBranchingStrategy/obj/Debug/net8.0/DemoBranchingStrategy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoBranchingStrategy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b68ca9770c8610e0d7e9c21b652f13ba5a7b1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoBranchingStrategy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoBranchingStrategy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
